331432614705f29483bdca96f150172f90120ce4
00mab <ayan.butt@antino.com>
2024-12-20 17:12:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and 16 should run now
+          Edit <code>src/App.js</code> changed color
         </p>
         <a
           className="App-link"
